a92d10f3f7b1859c7cb45c80cb2e197e8394b2e6
cyc <chenyc@shanghaitech.edu.cn>
2021-02-28 00:45:10 +0800

    v1_cyc

---
+++ Lab1/cmake-build-debug/CMakeFiles/Lab1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab1.dir/main.c.o"
+  "Lab1"
+  "Lab1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Lab1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
