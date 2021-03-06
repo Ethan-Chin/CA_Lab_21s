3132d3b40a92fe6b3e7ebf1797eb159388a53a39
cyc <chenyc@shanghaitech.edu.cn>
2021-03-07 02:17:20 +0800

    ex2

--- Lab2/hello.c
+++ Lab2/hello.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+
+void func()
+{
+    for (int i = 0; i < 10; ++i) {
+        printf("%d", i);
+    }
+    printf("func!");
+}
+
 int main (int argc, char *argv[])
 {
   int i, *p, count = 0;
@@ -9,6 +18,10 @@ int main (int argc, char *argv[])
   	(*p)++;	//Do you understand this line of code and all the other permutations of the operators? ;)
   }
 
-  printf("Thanks for waddling through this program. Have a nice day.");
+
+  func();
+
+
+  printf("Thanks for waddling through this program. Have a nice day.\n");
   return 0;
 }
