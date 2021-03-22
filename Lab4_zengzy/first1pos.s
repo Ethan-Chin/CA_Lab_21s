bcda9728e4366432d62a26aa3bbd1a09c58a3b40
Adonis-galaxy <57528157+Adonis-galaxy@users.noreply.github.com>
2021-03-22 21:27:29 +0800

    Just changed and added some comments

---
+++ Lab4_zengzy/first1pos.s
@@ -0,0 +1,128 @@
+.data
+shouldben1:	.asciiz "Should be -1, and firstposshift and firstposmask returned: "
+shouldbe0:	.asciiz "Should be 0 , and firstposshift and firstposmask returned: "
+shouldbe16:	.asciiz "Should be 16, and firstposshift and firstposmask returned: "
+shouldbe31:	.asciiz "Should be 31, and firstposshift and firstposmask returned: "
+
+.text
+main:
+	la	a1, shouldbe31
+	jal	print_str
+	lui	a1, 0x80000	# should be 31
+	jal	first1posshift
+	mv	a1, a0
+	jal	print_int
+	jal	print_space
+
+	lui	a1, 0x80000
+	jal	first1posmask
+	mv	a1, a0
+	jal	print_int
+	jal	print_newline
+
+	la	a1, shouldbe16
+	jal	print_str
+	lui	a1, 0x0010	# should be 16
+	jal	first1posshift
+	mv	a1, a0
+	jal	print_int
+	jal	print_space
+
+	lui	a1, 0x0010
+	jal	first1posmask
+	mv	a1, a0
+	jal	print_int
+	jal	print_newline
+
+	la	a1, shouldbe0
+	jal	print_str
+	li	a1, 1		# should be 0
+	jal	first1posshift
+	mv	a1, a0
+	jal	print_int
+	jal	print_space
+
+	li	a1, 1
+	jal	first1posmask
+	mv	a1, a0
+	jal	print_int
+	jal	print_newline
+
+	la	a1, shouldben1
+	jal	print_str
+	mv	a1, zero		# should be -1
+	jal	first1posshift
+	mv	a1, a0
+	jal	print_int
+	jal	print_space
+
+	mv	a1, zero
+	jal	first1posmask
+	mv	a1, a0
+	jal	print_int
+	jal	print_newline
+
+	li	a0, 10
+	ecall
+
+first1posshift:
+
+    beq a1, zero, return0
+    
+    li  t0, 31
+    
+loop_shift:
+    blt a1, zero, returnposition # <0 means the number is negative, the sign position is 1
+    slli    a1, a1, 1	
+    addi    t0, t0, -1	# t0 is the times that shift left
+    j loop_shift
+
+
+
+first1posmask:
+
+    beq a1, zero, return0
+
+    li  t0, 31
+    li  t1, 0x80000000
+
+loop_mask:
+    and t2, t1, a1
+    bne t2, zero, returnposition
+    srli    t1, t1, 1 # every time shift t1
+    addi    t0, t0, -1
+    j loop_mask
+	
+
+
+return0:
+    addi    a0, zero, -1
+    jr  ra
+
+returnposition:
+    mv  a0, t0
+    jr  ra
+
+
+print_int:
+	mv	a1, a0
+	li	a0, 1
+	ecall
+	jr	ra
+
+print_str:
+	li	a0, 4
+	ecall
+	jr	ra
+
+print_space:
+	li	a1, ' '
+	li	a0, 11
+	ecall
+	jr	ra
+
+print_newline:
+	li	a1, '\n'
+	li	a0, 11
+	ecall
+	jr	ra
---
+++ Lab4_zengzy/listmanips.s
@@ -0,0 +1,119 @@
+.data
+start_msg:	.asciiz "List before: "
+end_msg:	.asciiz "List after: "
+
+.text
+main:	jal	create_default_list # ra point to here when call jal, tag 2
+	add	s1, a0, zero	# s1=a0 is head of node list, a0 point to the last added node(iq)
+
+	# print "list before: "
+	la	a1, start_msg
+	li	a0, 4
+	ecall
+
+	# print the list
+	add	a1, s1, zero
+	jal	print_list
+
+	# print a newline
+	jal	print_newline
+
+	# issue the map call
+	add	a1, s1, zero	# load the address of the first node into a1
+
+
+# load the address of the function into a2 (check out la)
+	la  a2, square  
+
+
+	jal	map # tag 3
+
+	# print "list after: "
+	la	a1, end_msg
+	li	a0, 4
+	ecall
+
+	# print the list
+	add	a1, s1, zero
+	jal	print_list
+	li	a0, 10
+	ecall
+
+map:
+	addi	sp, sp, -12
+	sw	ra, 0(sp) # 0(sp) to tag 3
+	sw	s2, 4(sp)
+	sw	s1, 8(sp)
+
+	beq	a1, zero, done	# if we were given a null pointer, we're done.
+
+	add	s1, a1, zero	# save address of this node in s1
+	add	s2, a2, zero	# save address of function in s2
+
+	# remember that each node is 8 bytes long: 4 for the value followed by 4 for the pointer to next
+	# load the value of the current node into a1
+	lw  a1, 0(s1)
+	# call the function on that value.
+	jalr    s2 # to square
+	# store the returned value back into the node
+	sw  a0, 0(s1)
+	# load the address of the next node into a1
+	lw  a1, 4(s1)
+	# put the address of the function back into a2 to prepare for the recursion
+	add a2, s2, zero
+	# recurse
+	jal map
+
+done:
+	lw	s1, 8(sp)
+	lw	s2, 4(sp)
+	lw	ra, 0(sp) 
+	addi	sp, sp, 12
+	jr	ra # to tag 3
+
+square:
+	mul	a0 ,a1, a1
+	jr	ra
+
+create_default_list:
+	addi	sp, sp, -4
+	sw	ra, 0(sp)
+	li	s1, 0		# pointer to the last node we handled
+	li	s2, 0		# number of nodes handled,i 
+loop:					#do...
+	li	a1, 8
+	jal	malloc		# get memory for the next node, 1 jump point
+	sw	s2, 0(a0)		# node->value = i, update *a0, there are 8 byte start in a0
+	sw	s1, 4(a0)		# node->next = last
+	add	s1, zero, a0	# last = node
+	addi	s2, s2, 1		# i++
+    	addi    t1, s2, -10
+	bne	t1, zero, loop	# ... while i!= 10
+	lw	ra, 0(sp) # done create_default_list
+	addi	sp, sp, 4
+	jr	ra # jump to tag 2
+
+print_list:
+	bne	a1, zero, printMeAndRecurse
+	jr	ra 			# nothing to print
+printMeAndRecurse:
+	add	t0, a1, zero	# t0 gets current node address
+	lw	a1, 0(t0)		# a0 gets value in current node
+	li	a0, 1		# preparte for print integer ecall
+	ecall
+	li	a1, ' '		# a0 gets address of string containing space
+	li	a0, 11		# prepare for print string ecall
+	ecall
+	lw	a1, 4(t0)		# a0 gets address of next node
+	j	print_list		# recurse. We don't have to use jal because we already have where we want to return to in ra
+
+print_newline:
+	li	a1, '\n'
+	li	a0, 11
+	ecall
+	jr	ra
+
+malloc:	# store 8 byte in heap and return pointer to it in a0
+	li	a0, 9
+	ecall
+	jr	ra # jump to 1
