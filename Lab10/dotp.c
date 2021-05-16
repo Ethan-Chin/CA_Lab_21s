0192f86d1f65a89287d3bc755d5f5095a3fdfd97
Adonis-galaxy <57528157+Adonis-galaxy@users.noreply.github.com>
2021-05-16 16:53:39 +0800

    Lab10 complete

---
+++ Lab10/dotp.c
@@ -0,0 +1,154 @@
+#include <math.h>
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define REPEAT 100
+
+int array_size = 10000000;
+
+double *
+gen_array (int n)
+{
+  double *array = malloc(sizeof(*array) * n);
+  for (int i = 0; i < n; i++)
+    array[i] = drand48 ();
+  return array;
+}
+
+double
+dotp_naive (double *x, double *y, int arr_size)
+{
+  double global_sum = 0.0;
+  #pragma omp parallel
+  {
+    #pragma omp for
+    for (int i = 0; i < arr_size; i++)
+      #pragma omp critical
+      global_sum += x[i] * y[i];
+  }
+  return global_sum;
+}
+
+// EDIT THIS FUNCTION PART 1
+double
+dotp_manual_optimized (double *x, double *y, int arr_size)
+{
+  double global_sum = 0.0;
+  #pragma omp parallel
+  {
+    double private_sum=0;
+    int thread_ID = omp_get_thread_num ();
+    int thread_num = omp_get_num_threads();
+    int chunk_size=arr_size/thread_num; //1000 / 10 = 100
+    int res_size=arr_size%thread_num; //add to the last thread
+    int begin=chunk_size*thread_ID; //100*0=0,0-99(<100),100-199,...,900-999(<1000)+re_size
+    //calculate this chunk
+    for (int i = begin; i < begin+chunk_size; i++)
+    {
+      private_sum=private_sum+ x[i] * y[i];
+    }
+    //deal with res block
+    if (thread_ID==thread_num-1)
+    {
+      for (int i = begin+chunk_size; i < begin+chunk_size+res_size; i++)
+      {
+        private_sum=private_sum+ x[i] * y[i];
+      }
+    }
+    #pragma omp critical
+    global_sum+=private_sum;
+  }
+  return global_sum;
+}
+
+// EDIT THIS FUNCTION PART 2
+double
+dotp_reduction_optimized (double *x, double *y, int arr_size)
+{
+  double global_sum = 0.0;
+#pragma omp parallel  for reduction(+: global_sum)
+    for (int i = 0; i < arr_size; i++)
+    {
+      // #pragma omp critical
+      global_sum = global_sum+x[i] * y[i];
+    }
+
+  return global_sum;
+}
+
+int main ()
+{
+  // Generate input vectors
+  double *x = gen_array(array_size), *y = gen_array(array_size);
+  double start_time, run_time;
+  double serial_result, result;
+  int ret = 0;
+
+  // // calculate result serially
+  start_time = omp_get_wtime ();
+  for (int j = 0; j < REPEAT; j++)
+    {
+      serial_result = 0.0;
+      for (int i = 0; i < array_size; i++)
+        serial_result += x[i] * y[i];
+    }
+  run_time = omp_get_wtime () - start_time;
+  printf ("Naive solution took %f seconds\n", run_time);
+
+  int num_threads = omp_get_max_threads ();
+  for (int i = 1; i <= num_threads; i++)
+    {
+      omp_set_num_threads (i);
+      start_time = omp_get_wtime ();
+
+      for (int j = 0; j < REPEAT; j++)
+        result = dotp_manual_optimized (x, y, array_size);
+
+      run_time = omp_get_wtime() - start_time;
+      printf ("Manual Optimized: %d thread(s) took %f seconds\n", i, run_time);
+
+      // verify result is correct (within some threshold)
+      if (fabs (serial_result - result) > 0.001)
+        {
+          printf("Incorrect result!\n");
+          ret = -1;
+          goto exit;
+        }
+    }
+
+  // for (int i = 1; i <= num_threads; i++)
+  //   {
+  //     omp_set_num_threads (i);
+  //     start_time = omp_get_wtime ();
+
+  //     for (int j = 0; j < REPEAT; j++)
+  //       result = dotp_reduction_optimized (x, y, array_size);
+
+  //     run_time = omp_get_wtime () - start_time;
+  //     printf ("Reduction Optimized: %d thread(s) took %f seconds\n", i, run_time);
+
+  //     // verify result is correct (within some threshold)
+  //     if (fabs (serial_result - result) > 0.001)
+  //       {
+  //         printf("Incorrect result!\n");
+  //         ret = -1;
+  //         goto exit;
+  //       }
+  //   }
+
+  // // Only run this once because it's too slow..
+  // omp_set_num_threads (1);
+  // start_time = omp_get_wtime ();
+  // for (int j = 0; j < REPEAT; j++)
+  //   result = dotp_naive (x, y, array_size);
+  // run_time = omp_get_wtime () - start_time;
+
+  // printf ("Naive: %d thread(s) took %f seconds\n", 1, run_time);
+
+ exit:
+  free (x);
+  free (y);
+  return ret;
+}
